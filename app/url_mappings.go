d80f37640c59f0cf94793e528ba861ca935edb64
Micah Jackson <gmicahjackson@gmail.com>
2021-12-03 15:00:04 -0800

    hide search user while undefined

--- app/url_mappings.go
+++ app/url_mappings.go
@@ -9,6 +9,6 @@ func mapUrls() {
 	router.GET("/ping", ping.Ping)
 
 	router.GET("/users/:user_id", users.GetUser)
-	router.GET("/users/search", users.SearchUser)
+	// router.GET("/users/search", users.SearchUser)
 	router.POST("/users", users.CreateUser)
 }
