6bd790744161f1935496571c53acb92338d09b3d
Micah Jackson <gmicahjackson@gmail.com>
2021-12-03 08:33:54 -0800

    trying to get the implement message to return

--- app/url_mappings.go
+++ app/url_mappings.go
@@ -8,6 +8,6 @@ func mapUrls() {
 	router.GET("/ping", controllers.Ping)
 
 	router.GET("/users/:user_id", controllers.GetUser)
-	router.GET("/users/search", controllers.SearchUser)
+	// router.GET("/users/search", controllers.SearchUser)
 	router.POST("/users", controllers.CreateUser)
 }
