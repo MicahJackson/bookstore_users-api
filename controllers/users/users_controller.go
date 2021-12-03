7b3850581425a68292b92b82ce4aee2f7d8ab8a5
Micah Jackson <gmicahjackson@gmail.com>
2021-12-03 15:46:08 -0800

    creating users

--- controllers/users/users_controller.go
+++ controllers/users/users_controller.go
@@ -7,16 +7,24 @@ package users
 // application and controllers are the only layers that should interact with the server
 
 import (
+	"fmt"
+	"io/ioutil"
 	"net/http"
 
 	"github.com/gin-gonic/gin"
+	"github.com/micahjackson/bookstore_users-api/domain/users"
 )
 
-func GetUser(c *gin.Context) {
+func CreateUser(c *gin.Context) {
+	var user users.User
+	fmt.Println(user)
+	bytes, err := ioutil.ReadAll(c.Request.Body)
+	fmt.Println(err)
+	fmt.Println(string(bytes))
 	c.String(http.StatusNotImplemented, "implement me!")
 }
 
-func CreateUser(c *gin.Context) {
+func GetUser(c *gin.Context) {
 	c.String(http.StatusNotImplemented, "implement me!")
 }
 
