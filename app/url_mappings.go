f28686de2f32f52ee55058330e0a2d2635d957e7
Micah Jackson <gmicahjackson@gmail.com>
2021-12-02 18:26:06 -0800

    testing gin readme ping pong function

--- app/url_mappings.go
+++ app/url_mappings.go
@@ -1,9 +1,16 @@
 package app
 
 import (
-	"github.com/micahjackson/bookstore_users-api/controllers"
+	// "github.com/micahjackson/bookstore_users-api/controllers"
+	"github.com/gin-gonic/gin"
 )
 
 func mapUrls() {
-	router.GET("/ping", controllers.Ping)
+	// router.GET("/ping", controllers.Ping)
+
+	router.GET("/ping", func(c *gin.Context) {
+		c.JSON(200, gin.H{
+			"message": "pong",
+		})
+	})
 }
