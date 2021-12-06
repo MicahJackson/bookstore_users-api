9bba11528f1a9a86845736b100536f5fb771168a
Micah Jackson <gmicahjackson@gmail.com>
2021-12-06 10:47:48 -0800

    update date_utils

--- utils/date_utils/date_utils.go
+++ utils/date_utils/date_utils.go
@@ -8,7 +8,10 @@ const (
 	apiDateLayout = "2006-01-02T15:04:05Z"
 )
 
+func GetNow() time.Time {
+	return time.Now().UTC()
+}
+
 func GetNowString() string {
-	now := time.Now().UTC()
-	return now.Format(apiDateLayout)
+	return GetNow().Format(apiDateLayout)
 }
